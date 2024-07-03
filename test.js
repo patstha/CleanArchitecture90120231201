6a3ec8b3c08288f40de693895da8741cce390ba7
Kushal <kushaldeveloper@gmail.com>
2024-07-02 23:18:51 -0400

    add all files
    from terminal

---
+++ test.js
@@ -0,0 +1,59 @@
+// In Purrfect Code Level 3, try using JavaScript functions
+// to solve the puzzle. 
+//Move one space north
+//moveNorth();
+
+//Move one space south
+//moveSouth();
+
+//Move one space east
+//moveEast();
+
+//Move one space west
+//moveWest();
+
+moveSouth();
+moveSouth();
+moveEast();
+moveEast();
+moveEast();
+moveNorth();
+moveNorth();
+moveWest();
+moveWest();
+moveNorth();
+moveWest();
+moveSouth();
+
+moveEast();
+moveEast();
+moveNorth();
+moveNorth();
+moveNorth();
+moveNorth();
+moveNorth();
+moveWest();
+moveWest();
+
+moveEast();
+moveEast();
+moveEast();
+moveSouth();
+moveSouth();
+moveSouth();
+moveSouth();
+moveSouth();
+moveEast();
+moveEast();
+moveNorth();
+moveNorth();
+moveNorth();
+moveNorth();
+moveNorth();
+
+moveWest();
+moveWest();
+moveWest();
+
+//Activate the teleporter to solve the level
+activateTeleporter();
